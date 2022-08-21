c2bcd36d3882c3c1eff1d1efdeb9bd99e0eaaabc
Brent <brenton.crowley@gmail.com>
2022-08-22 09:50:14 +1000

    Initial Commit

---
+++ WorkoutCardsSwiftUI/WorkoutCardsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutCardsSwiftUIApp.swift
+//  WorkoutCardsSwiftUI
+//
+//  Created by Brent on 22/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutCardsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
